e80129bd9d46a07d0b9cf5e2def3f2ca22c3d8dd
Daniel Cruz <d.afc98@gmail.com>
2026-02-16 16:39:15 -0300

    <Feat>[Q3] - Solution MaxConsecutiveOnes_Q3

---
+++ MaxConsecutiveOnes_Q3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MaxConsecutiveOnes_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums1 = new int[] { 1, 1, 0, 1, 1, 1 };
+            var output1 = FindMaxConsecutiveOnes(nums1);
+            Console.WriteLine(output1);
+
+            var num2 = new int[] { 1, 0, 1, 1, 0, 1 };
+            var output2 = FindMaxConsecutiveOnes(num2);
+            Console.WriteLine(output2);
+        }
+
+        private static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var maxCount = 0;
+            var count = 0; 
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                    maxCount = Math.Max(maxCount, count);
+                    continue;
+                }
+
+                maxCount = Math.Max(maxCount, count);
+                count = 0;
+            }
+
+            return maxCount;
+        }
+    }
+}
