2f12cb6a0662fac59c105f1e5672c5f21873b33b
Daniel Cruz <d.afc98@gmail.com>
2026-05-20 21:06:46 -0300

    feat: two sun solution

---
+++ TwoSum_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TwoSum_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = [2,7,11,15];
+        var result = TwoSum(nums, 9);
+        Console.WriteLine($"[{result[0]}, {result[1]}]");
+    }
+    
+    private static int[] TwoSum(int[] nums, int target) 
+    {
+        var numDict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+
+            if(numDict.ContainsKey(complement))
+            {
+                return new int[] { numDict[complement], i };
+            }
+
+            numDict[nums[i]] = i;
+        }
+        return Array.Empty<int>();
+    }
+}
