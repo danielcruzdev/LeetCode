4024b88abd8d7094f0e3ab13c36402a3204f9869
Daniel Cruz <d.afc98@gmail.com>
2026-06-04 16:06:57 -0300

    <feat>[3090] - MaximumLengthSubstringWithTwoOccurrences_3090 solution

---
+++ MaximumLengthSubstringWithTwoOccurrences_3090/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace MaximumLengthSubstringWithTwoOccurrences_3090;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s = "bcbbbcba";
+        var result = MaximumLengthSubstring(s);
+        Console.WriteLine(result);
+    }
+    
+    private static int MaximumLengthSubstring(string s)
+    {
+        var right = 0;
+        var left = 0;
+        var max = 1;
+        
+        var counter = new Dictionary<char, int>
+        {
+            [s[0]] = 1
+        };
+
+        while (right < s.Length - 1)
+        {
+            right++;
+            if(counter.ContainsKey(s[right]))
+                counter[s[right]]++;
+            else
+                counter[s[right]] = 1;
+            
+            while (counter[s[right]] == 3)
+            {
+                counter[s[left]]--;
+                left++;
+            }
+            
+            max = Math.Max(max, right - left + 1);
+        }
+        
+        return max;
+    }
+}
