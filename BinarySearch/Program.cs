d7e577363753a3341ab9d387fd6357c6175ffe24
Daniel Cruz <d.afc98@gmail.com>
2026-06-08 21:12:44 -0300

    binary search and reverse words

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BinarySearch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var target = 7;
+        var result =  BinarySearch(array, target);
+        Console.WriteLine("Steps: " + result.steps + " | " + "Index: " + result.index);
+
+        array = new int[] { 3, 8, 12, 17, 23, 29, 34, 41, 47, 52, 58, 63, 69, 74, 81, 87, 93, 99, 105, 112, 118, 124, 130, 137, 143, 149, 156, 162, 168, 175 };
+        target = 99;
+        result =  BinarySearch(array, target);
+        Console.WriteLine("Steps: " + result.steps + " | " + "Index: " + result.index);
+
+        array = new int[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30 };
+        target = 30;
+        result =  BinarySearch(array, target);
+        Console.WriteLine("Steps: " + result.steps + " | " + "Index: " + result.index);
+
+        array = new int[] { -50, -20, -10, -5, 0, 5, 10, 20, 50 };
+        target = 12;
+        result =  BinarySearch(array, target);
+        Console.WriteLine("Steps: " + result.steps + " | " + "Index: " + result.index);
+    }
+
+    private static (int steps, int index) BinarySearch(int[] array, int target)
+    {
+        var lo = 0;
+        var hi = array.Length;
+        var steps = 0;
+        
+        while(lo <= hi)
+        {
+            steps++;
+            var mid = (lo + hi) / 2;
+            if (array[mid] == target)
+                return (steps, mid);
+            else if (array[mid] < target)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        
+        return (steps, -1);
+    }
+}
---
+++ ReverseWordsString_557/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace ReverseWordsString_557;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s = "Let's take LeetCode contest";
+        var result = ReverseWords(s);
+        Console.WriteLine(result);
+    }
+    
+    private static string ReverseWords(string s)
+    {
+        var answer = new StringBuilder();
+        var right = 0; 
+        var left = 0;
+        
+        while (right < s.Length)
+        {
+            if (s[right] == ' ')
+            {
+                answer.Append(s.Substring(left, right - left + 1).Reverse().ToArray());
+                left = right + 1;
+            }
+            right++;
+        }
+        
+        answer.Append(s.Substring(left, right - left).Reverse().ToArray());
+        return answer.Remove(0, 1).ToString();
+    }
+}
