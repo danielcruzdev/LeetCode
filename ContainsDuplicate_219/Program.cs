2ce88f70076191512f587f692b45ff53d8f66bd8
Daniel Cruz <d.afc98@gmail.com>
2026-06-18 22:06:25 -0300

    training sliding windows and hash map

---
+++ ContainsDuplicate_219/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConstainsDuplicate_219;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new int[] { 1, 2, 3, 1 };
+        var k = 3;
+        var result = ContainsNearbyDuplicate(nums, k);
+        Console.WriteLine(result);
+
+        nums = new int[] {1,2,3,1,2,3};
+        k = 2;
+        result = ContainsNearbyDuplicate(nums, k);
+        Console.WriteLine(result);
+
+        nums = new int[] {1,2,3,4,5,6,7,8,9,9};
+        k = 3;
+        result = ContainsNearbyDuplicate(nums, k);
+        Console.WriteLine(result);
+    }
+    
+    private static bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var set = new HashSet<int>(k + 1);
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            if (set.Count > k)
+                set.Remove(nums[i - k - 1]);
+            if (!set.Add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ ExponentialSearch/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ExponentialSearch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        var target = 15;
+        var result = ExponentialSearch(nums, target);
+        Console.WriteLine(result);
+    }
+    
+    private static int ExponentialSearch(int[] nums, int target)
+    {
+        if (nums[0] == target)
+            return 0;
+
+        int bound = 1;
+
+        while (bound < nums.Length && nums[bound] < target)
+        {
+            bound *= 2;
+        }
+
+        int left = bound / 2;
+        int right = Math.Min(bound, nums.Length - 1);
+
+        return BinarySearch(nums, target, left, right);
+    }
+
+    private static int BinarySearch(
+        int[] nums,
+        int target,
+        int left,
+        int right)
+    {
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+}
