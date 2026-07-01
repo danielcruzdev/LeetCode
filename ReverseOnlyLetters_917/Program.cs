3135af0c9dd502783168a9ec7137d3c547953a87
Daniel Cruz <d.afc98@gmail.com>
2026-06-30 21:33:02 -0300

    ReverseOnlyLetters_917.csproj solution

---
+++ ReverseOnlyLetters_917/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace ReverseOnlyLetters_917;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s = "ab-cd";
+        var result = ReverseOnlyLetters(s);
+        Console.WriteLine(result); //Output: "dc-ba"
+
+        s = "a-bC-dEf-ghIj";
+        result = ReverseOnlyLetters(s);
+        Console.WriteLine(result); //Output: "j-Ih-gfE-dCba"
+
+        s = "Test1ng-Leet=code-Q!";
+        result = ReverseOnlyLetters(s);
+        Console.WriteLine(result); //Output: "Qedo1ct-eeLg=ntse-T!"
+    }
+    
+    private static string ReverseOnlyLetters(string s)
+    {
+        var left = 0; 
+        var right = s.Length-1;
+        var builder = new StringBuilder(s);
+        
+        while(left<right){
+            
+            if(!char.IsLetter(s[left]))
+            {
+                left++;
+                continue;
+            }
+            
+            if(!char.IsLetter(s[right]))
+            {
+                right--;
+                continue;
+            }
+
+            (builder[left], builder[right]) = (builder[right], builder[left]);
+            left++;
+            right--;
+        }
+        return builder.ToString();
+    }
+}
